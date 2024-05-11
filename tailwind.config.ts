4c6b0d2dfd3f77f71f33623b2c43ec52672c69f1
abdulkarim <abdulkarimogaji001@gmail.com>
2024-05-11 12:28:23 +0100

    header and footer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,21 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      borderRadius: { circle: "50%", pill: "100vw" },
+      colors: {
+        olive: "#5F6F52",
+        graphite: "#303030",
+        "coffee-supreme": "#A9B388",
+        snow: "#F3F3F3",
+        "sugar-cream": "#FEFAE0",
+        nougat: "#B99470",
       },
     },
   },
